323b057a6dc0939d18bf2c61e826279ac50548ba
jaepil <jaepill94@gmail.com>
2025-12-04 16:38:00 +0900

    env 세분화

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 function App() {
   const greeting = import.meta.env.VITE_TEXT_GREETING;
+  const mode = import.meta.env.MODE;
 
   return (
     <>
-      <h1>Hello React@@@@</h1>
-      <p>{greeting}</p>
+      <h1>Hello React</h1>
+      <p>
+        <strong>Current Mode:</strong> {mode}
+      </p>
+      <p>
+        <strong>Greeting:</strong> {greeting}
+      </p>
     </>
   );
 }
